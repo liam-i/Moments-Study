f054fa4ab1b7b267d564bb07d7c54e8ca21a6c67
yalla012 <li.peng@yalla.live>
2021-04-01 11:09:08 +0800

    first ci.

---
+++ Moments/Moments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moments
+//
+//  Created by yalla012 on 2021/3/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
